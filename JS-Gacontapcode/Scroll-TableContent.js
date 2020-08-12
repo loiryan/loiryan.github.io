5391028cbe9a423e1b1d0622f4e9912562a5627d
Lợi Ryan <35736790+loiryan@users.noreply.github.com>
2020-08-12 20:27:03 +0700

    Delete Scroll-TableContent.js

--- JS-Gacontapcode/Scroll-TableContent.js
+++
@@ -1,2 +0,0 @@
-//TableContent         
-$(document).ready(function(){$(".post-body a").on("click",function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},600,function(){window.location.hash=t})}})});
