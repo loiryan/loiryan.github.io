60833b8fa676d7e1dc1cfe9b351405afde66bafc
Lợi Ryan <35736790+loiryan@users.noreply.github.com>
2020-08-12 20:27:19 +0700

    Delete popup-donate.js

--- JS-Gacontapcode/popup-donate.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($){
-$('.popup-trigger').on('click', function(event){event.preventDefault();$('.popup').addClass('is-visible');});	
-$('.popup').on('click', function(event){if( $(event.target).is('.popup-close') || $(event.target).is('.popup') ) {event.preventDefault();$(this).removeClass(&#39;is-visible&#39;);}});
-$(document).keyup(function(event){if(event.which=='27'){$('.popup').removeClass('is-visible');}});});
