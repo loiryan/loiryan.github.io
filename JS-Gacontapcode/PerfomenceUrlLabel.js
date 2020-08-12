d786b036b1b1946517cdf16ccbfd2dc6978b75ef
Lợi Ryan <35736790+loiryan@users.noreply.github.com>
2020-08-12 10:30:13 +0700

    Delete PerfomenceUrlLabel.js

--- JS-Gacontapcode/PerfomenceUrlLabel.js
+++
@@ -1,2 +0,0 @@
-//PerfomenceUrlLabel
-var tnreplace=function(a){return a.replace(/([\/-])?thu-thuat-blogger/gi,function(b,c){return c?b:"Thủ thuật Blogger"}).replace(/([\/-])?phan-mem-may-tinh/gi,function(b,c){return c?b:"Phần mềm máy tính"}).replace(/([\/-])?hosting-domain/gi,function(b,c){return c?b:"Hosting"})},mytitle=document.title;document.title=tnreplace(mytitle);var mybody=document.body.innerHTML;document.body.innerHTML=tnreplace(mybody);
